37b2a0217d91df14c4045ab156529441af1ace76
Anoop Singh <Reggerfgod@gmail.com>
2025-05-10 16:54:06 +0530

    CHANGELOG [Version: 1.0.3]
    ###BUGFIX
    - ClassNotFoundException
    - Emerald Gen

--- src/main/java/com/reggarf/mods/create_dimension/registry/ModTrades.java
+++ src/main/java/com/reggarf/mods/create_dimension/registry/ModTrades.java
@@ -3,7 +3,9 @@
 package com.reggarf.mods.create_dimension.registry;
 
 
-import com.reggarf.mods.create_better_villagers.init.CreateBetterVillagersModVillagerProfessions;
+
+import com.reggarf.mods.create_better_villagers.config.ModConfigs;
+import com.reggarf.mods.create_better_villagers.init.ModVillagers;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.village.VillagerTradesEvent;
@@ -16,10 +18,12 @@ import net.minecraft.world.item.ItemStack;
 public class ModTrades {
 	@SubscribeEvent
 	public static void registerTrades(VillagerTradesEvent event) {
-		if (event.getType() == CreateBetterVillagersModVillagerProfessions.ANDESITE_WORKER.get()) {
+		if (ModConfigs.ENABLE_ANDESITE_WORKER.get()) {
+		if (event.getType() == ModVillagers.ANDESITE_WORKER.get()) {
 			event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 3),
 
 					new ItemStack(ModItems.STEAMWORKS_REALM.get()), 2, 10, 0.06f));
+		   }
 		}
 	}
 }
