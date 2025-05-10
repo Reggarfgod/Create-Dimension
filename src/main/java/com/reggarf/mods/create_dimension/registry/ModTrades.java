c69cc57b286484fda24c92dd3a9b967a6a6ae131
Anoop Singh <Reggerfgod@gmail.com>
2025-05-10 19:15:04 +0530

    CHANGELOG [Version: 1.0.3]
    ###BUGFIX
    - ClassNotFoundException
    - Emerald Gen

--- src/main/java/com/reggarf/mods/create_dimension/registry/ModTrades.java
+++ src/main/java/com/reggarf/mods/create_dimension/registry/ModTrades.java
@@ -1,6 +1,8 @@
 package com.reggarf.mods.create_dimension.registry;
 
-import com.reggarf.mods.create_better_villagers.init.ModVillagerProfessions;
+import com.reggarf.mods.create_better_villagers.Create_better_villagers;
+
+import com.reggarf.mods.create_better_villagers.init.ModVillagers;
 import net.neoforged.neoforge.event.village.VillagerTradesEvent;
 import net.neoforged.neoforge.common.BasicItemListing;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -13,8 +15,10 @@ import net.minecraft.world.item.ItemStack;
 public class ModTrades {
 	@SubscribeEvent
 	public static void registerTrades(VillagerTradesEvent event) {
-		if (event.getType() == ModVillagerProfessions.ANDESITE_WORKER.get()) {
-			event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 3), new ItemStack(ModItems.STEAMWORKS_REALM.get()), 2, 5, 0.05f));
+		if (Create_better_villagers.CONFIG.common.ENABLE_ANDESITE_WORKER) {
+			if (event.getType() == ModVillagers.ANDESITE_WORKER.value()) {
+				event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 3), new ItemStack(ModItems.STEAMWORKS_REALM.get()), 2, 5, 0.05f));
+			}
 		}
 	}
 }
